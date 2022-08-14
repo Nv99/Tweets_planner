fd08e5b8dbe33457cb18eeae578eb5450cb36dfd
Naveen Verma <naveen.verma@cogoport.com>
2022-08-14 16:57:39 +0530

    TWITTER ACCOUNT ADDED

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,29 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "about", to: "about#index"
   # Defines the root path route ("/")
+
+  get "password", to: "passwords#edit", as: :edit_password
+  patch "password", to: "passwords#update"
+
   get "main", to: "main#index"
   # root "articles#index"
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+  delete "logout", to: "sessions#destroy"
+
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+
+  get "password/reset", to: "password_resets#new"
+  post "password/reset", to: "password_resets#create"
+
+  get "password/reset/edit", to: "password_resets#edit"
+  patch "password/reset/edit", to: "password_resets#update"
+
+  get "/auth/twitter/callback", to: "omniauth_callbacks#twitter" 
+
+  resources :twitter_accounts
+
+
+
 end
